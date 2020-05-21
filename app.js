077e0a1acd4d804f3b1fc284438e8789a8fcbab6
Shinji Obana <willshinji@gmail.com>
2020-05-21 17:47:53 +0100

    update README.md

--- app.js
+++ app.js
@@ -21,22 +21,42 @@ app.get('/newUser', (req, res) => {
     res.sendStatus(200);
 });
 
+// app.get('/auth', (req, res) => {
+//     let username = req.query.username || '';
+//     const password = req.query.password || '';
+
+//     username = username.replace(/[!@#$%^&*]/g, '');
+
+//     if (!username || !password || !users[username]) {
+//         return res.sendStatus(400);
+//     }
+
+//     const { salt, hash } = users[username];
+//     const encryptHash = crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512');
+
+//     if (crypto.timingSafeEqual(hash, encryptHash)) {
+//         res.sendStatus(200);
+//     } else {
+//         res.sendStatus(401);
+//     }
+// });
+
 app.get('/auth', (req, res) => {
     let username = req.query.username || '';
     const password = req.query.password || '';
-
+  
     username = username.replace(/[!@#$%^&*]/g, '');
-
+  
     if (!username || !password || !users[username]) {
-        return res.sendStatus(400);
+      return res.sendStatus(400);
     }
-
-    const { salt, hash } = users[username];
-    const encryptHash = crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512');
-
-    if (crypto.timingSafeEqual(hash, encryptHash)) {
+  
+    crypto.pbkdf2(password, users[username].salt, 10000, 512, 'sha512', (err, hash) => {
+      if (users[username].hash.toString() === hash.toString()) {
         res.sendStatus(200);
-    } else {
+      } else {
         res.sendStatus(401);
-    }
-});
+      }
+    });
+  });
+
